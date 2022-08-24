b3fa5001e8529d17019ae5a7dfd2d2b45ee79c83
Rakibul Islam <rakibul241221@gmail.com>
2022-08-25 02:46:56 +0600

    template string and arrow function added

---
+++ MD30-basic-es6-es2015/js/arrow.js
@@ -0,0 +1,50 @@
+// function expression
+const add1 = function add(first, second) {
+    const total = first + second;
+    return total;
+}
+
+//function expression with  annonymous function
+const add2 = function (first, second) {
+    const total = first + second;
+    return total;
+}
+
+const add3 = function (first, second) {
+    return first + second;
+}
+
+// arrow function 1 addition
+const add4 = (first, second) => first + second;
+
+// multiple parameter 
+const addition = (a, b, c, d, e) => a + b + c + d + e;
+
+//  arrow function 2 for multiply
+const add5 = (first, second) => first * second;
+
+// ?arrow function 3 division
+const add6 = (first, second) => first / second;
+
+// ? no parameter arrow function 
+const getPie = () => 3.1416
+
+// one parameter multiply
+const multiply = (number) => number * number;
+
+// one parameter simplified version 
+const multiply2 = number => number * number
+
+// multiline arrow function 
+const doMath = (a, b, c) => {
+    const firstSum = a + b;
+    const secondSum = b + c;
+    const thirdSum = a + c;
+    const sumResult = firstSum + secondSum + thirdSum;
+    const result = sumResult / 3;
+
+    return result;
+}
+
+const result = add5(12, 22);
+console.log(result)
---
+++ MD30-basic-es6-es2015/js/template.js
@@ -0,0 +1,30 @@
+const name1 = 'rahima';
+const nam2 = 'karimaa';
+
+
+
+// use backtick 
+
+const multilineText = `hii hii bye byee
+where are you from 
+are come from argentina 
+can i say something to you ?
+`
+console.log(multilineText)
+
+
+const a = 100;
+const b = 200;
+
+// without backtick
+const varMama = [1, 3, 5, 7, 8, 9]
+const sum = 'sum of ' + a + ' and ' + b + ' is ' + (a + b);
+console.log(sum)
+
+// ?with backtick 
+const sum2 = `sum of ${a} and ${b} is ${a + b} and length of var mama is ${varMama.length}`;
+console.log(sum2)
+
+
+
+
