37d6b8b9308df8c48b19482ddaf28cc546ec9e1a
Rakibul Islam <rakibul241221@gmail.com>
2022-08-25 00:37:40 +0600

    default parameter function added

---
+++ MD30-basic-es6-es2015/js/let-const-var.js
@@ -0,0 +1,21 @@
+let money = 25;
+money = 30;
+
+const bird = 'jaaaaaaaan pakhi';
+
+console.log(money)
+
+const numberList = [1, 3, 6, 8, 9, 6];
+
+numberList.push(100)
+numberList[2] = 200;
+console.log(numberList);
+
+let sum = 0;
+for (let i = 0; i < numberList.length; i++) {
+    const number = numberList[i];
+    sum = sum + number
+}
+
+console.log(sum)
+
